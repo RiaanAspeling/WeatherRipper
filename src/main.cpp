870354884581ae5f65f77366684375c59e909985
Riaan Aspeling <riaan.aspeling@sage.com>
2024-05-29 14:42:07 +0200

    Added all needed libraries to compile.

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,44 @@
 #include <Arduino.h>
-#include "./Display/SSD1306WireDisplay.h"
+// #include "./Display/SSD1306WireDisplay.h"
 
-TDisplayManager *display = new TDisplayManager();
+#include "WeatherSensorCfg.h"
+#include "WeatherSensor.h"
+#include "InitBoard.h"
+
+WeatherSensor ws;
+
+#define HELTEC_POWER_BUTTON
+#define HELTEC_WIRELESS_STICK
+#include <heltec_unofficial.h>
+
+// TDisplayManager *display = new TDisplayManager();
 
 void setup() {
-  Serial.begin(115200);
-
-  pinMode(LED_BUILTIN, OUTPUT);
-
-  display->Start();
-  display->ClearDisplay();
-
-  Serial.println("Starting...");
+  heltec_setup();
+  Serial.println("Serial works");
+  // Display
+  display.println("Display works");
+  // Radio
+  display.print("Radio ");
+  int state = radio.begin();
+  if (state == RADIOLIB_ERR_NONE) {
+    display.println("works");
+  } else {
+    display.printf("fail, code: %i\n", state);
+  }
+  // Battery
+  float vbat = heltec_vbat();
+  display.printf("Vbat: %.2fV (%d%%)\n", vbat, heltec_battery_percent(vbat));
 }
 
 void loop() {
-  display->ClearDisplay();
-  display->PrintText(0, 0, "Running");
-  display->Loop();
-  delay(1000);
+  heltec_loop();
+  // Button
+  if (button.isSingleClick()) {
+    display.println("Button works");
+    // LED
+    for (int n = 0; n <= 100; n++) { heltec_led(n); delay(5); }
+    for (int n = 100; n >= 0; n--) { heltec_led(n); delay(5); }
+    display.println("LED works");
+  }
 }
